fb566fcec637132a7d63c0e9fa66e657f8c198cb
Luigi Vincent <luigipvincent@gmail.com>
2020-03-09 11:37:02 -0400

    Working state, all desired functions present

--- app.py
+++ app.py
@@ -1,18 +1,23 @@
 import sys
-from PyQt5.QtWidgets import QMainWindow, QGridLayout, QPushButton, QInputDialog, QWidget, QAction, QApplication, QDesktopWidget
+import collections
+from PyQt5.QtWidgets import QMainWindow, QGridLayout, QPushButton, QDialog, QLabel, QLineEdit, QInputDialog, QWidget, QAction, QApplication, QDesktopWidget
+
+# TODO-P: Maybe add color for symbol. Alt: Use stylesheets.
+Player = collections.namedtuple('Player', ['name', 'symbol'])
 
 class TicTacToe(QMainWindow):
 
     def __init__(self):
         super().__init__()
-        self.xPlayer = 'X Player'
-        self.yPlayer = 'Y Player'
+        self.xPlayer = Player('X Player', 'X')
+        self.oPlayer = Player('O Player', 'O')
         self.currentPlayer = self.xPlayer
-        self.turnIndicator = f"{self.currentPlayer}'s Turn"
+        self.turnIndicator = f"{self.currentPlayer.name}'s Turn"
         self.initUI()
 
     def initUI(self):
-        self.setCentralWidget(TicTacToeBoard())
+        self.board = TicTacToeBoard(self)
+        self.setCentralWidget(self.board)
         self.initElements()
         self.setWindowTitle('Tic Tac Toe')
         self.resize(1200, 1200)
@@ -28,31 +33,78 @@ class TicTacToe(QMainWindow):
         self.statusBar.showMessage(self.turnIndicator)
 
     def initMenu(self):
+        gameMenu = self.menuBar().addMenu('Game')
+        gameMenu.addAction(self.resetAction())
         settingsMenu = self.menuBar().addMenu('Settings')
         settingsMenu.addAction(self.setNameAction())
         settingsMenu.addAction(self.TurnIndicatorToggle())
+
+    def resetAction(self):
+        action = QAction('Reset', self)
+        action.setShortcut('Ctrl+R')
+        action.setToolTip('Reset Game')
+        action.triggered.connect(self.reset)
+        return action
+    
+    def reset(self):
+        self.board.reset()
+        self.currentPlayer = self.xPlayer
+        self.updateIndicator()
+
+    def updateIndicator(self):
+        # TODO-Q: Does PyQt have parameter binding? Can I also use signals / slots to 'auto-update' variables / displayed text?
+        self.turnIndicator = f"{self.currentPlayer.name}'s Turn"
+        self.statusBar.showMessage(self.turnIndicator)
 
     def setNameAction(self):
         action = QAction('Set Name(s)', self)
         action.setShortcut('Ctrl+A')
         action.setStatusTip('Set Player Name(s)')
-        # TODO: action.triggered.connect(self.setNameDialog)
+        action.triggered.connect(self.setNameDialog)
         return action
 
+    def setNameDialog(self):
+        dialog = QDialog(self)
+        dialog.setWindowTitle('Set Player Name(s)')
+        dialog.resize(600, 200)
+        layout = QGridLayout()
+        xText = QLineEdit()
+        oText = QLineEdit()
+        layout.addWidget(QLabel(f'<b>{self.xPlayer.name}:</b>'), 0, 0)
+        layout.addWidget(xText, 0, 1)
+        layout.addWidget(QLabel(f'<b>{self.oPlayer.name}:</b>'), 1, 0)
+        layout.addWidget(oText, 1, 1)
+        dialog.setLayout(layout)
+        enterButton = QPushButton('Enter')
+        enterButton.clicked.connect(lambda: self.setName(xText.text(), oText.text()))
+        enterButton.clicked.connect(dialog.accept)
+        layout.addWidget(enterButton, 2, 0)
+        dialog.exec_()
+
+    def setName(self, xName, oName):
+        if xName:
+            self.xPlayer = self.xPlayer._replace(name=xName)
+        if oName:
+            self.oPlayer = self.oPlayer._replace(name=oName)
+        self.currentPlayer = self.xPlayer if self.currentPlayer is self.xPlayer else self.oPlayer
+        self.updateIndicator()
+
+    def buttonClicked(self):
+        button = self.sender()
+        button.capture(self.currentPlayer.symbol)
+        self.togglePlayer()
+
     def togglePlayer(self):
-        if self.currentPlayer is self.xPlayer:
-            self.currentPlayer = self.yPlayer
-        else:
-            self.currentPlayer = self.xPlayer
+        self.currentPlayer = self.oPlayer if self.currentPlayer is self.xPlayer else self.xPlayer
+        self.updateIndicator()
 
     def TurnIndicatorToggle(self):
         action = QAction('Turn Indicator', self, checkable=True)
         action.setShortcut('Ctrl+T')
         action.setChecked(True)
-        action.setStatusTip('Toggle Turn Indicator')
+        action.setStatusTip('Toggle Status Indicator')
         action.triggered.connect(self.toggleTurnIndicator)
         return action
-
     
     def toggleTurnIndicator(self, state):
         if state:
@@ -61,23 +113,78 @@ class TicTacToe(QMainWindow):
         else:
             self.statusBar.hide()
 
+    def showVictory(self):
+        winner = self.oPlayer if self.currentPlayer is self.xPlayer else self.xPlayer
+        self.statusBar.showMessage(f'{winner.name} wins!')
+
     def center(self):
         qr = self.frameGeometry()
         qr.moveCenter(QDesktopWidget().availableGeometry().center())
         self.move(qr.topLeft())
 
+
 class TicTacToeBoard(QWidget):  
 
-    def __init__(self):
-        super().__init__()
-        grid = QGridLayout()
-        self.setLayout(grid)
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.game = parent
+        layout = QGridLayout()
+        self.setLayout(layout)
+        # TODO-Q: How to implement __mul__ so this works -> [TicTacToeCell(self) * 3] * 3
+        self.grid = [[TicTacToeCell(self), TicTacToeCell(self), TicTacToeCell(self)], [TicTacToeCell(self), TicTacToeCell(self), TicTacToeCell(self)], [TicTacToeCell(self), TicTacToeCell(self), TicTacToeCell(self)]]
         positions = [(i, j) for i in range(3) for j in range(3)]
         for position in positions:
-            button = QPushButton('')
-            button.setMinimumSize(400, 400)
-            grid.addWidget(button, *position)
+            x, y = position
+            button = self.grid[x][y]
+            button.clicked.connect(parent.buttonClicked)
+            button.clicked.connect(self.evaluateBoard)
+            layout.addWidget(button, *position)
+    
+    def reset(self):
+        for row in self.grid:
+            for cell in row:
+                cell.reset()
+    
+    def disable(self):
+        for row in self.grid:
+            for cell in row:
+                cell.setEnabled(False)
+    
+    def evaluateBoard(self):
+        self.evaluateCells(self.grid[0][0], self.grid[1][1], self.grid[2][2])
+        self.evaluateCells(self.grid[0][2], self.grid[1][1], self.grid[2][0])
+        for i in range(3):
+            self.evaluateCells(self.grid[i][0], self.grid[i][1], self.grid[i][2])
+            self.evaluateCells(self.grid[0][i], self.grid[1][i], self.grid[2][i])
 
+    def evaluateCells(self, a, b, c):
+        if a.captured() and a.text is b.text is c.text:
+            self.disable()
+            self.game.showVictory()
+
+
+class TicTacToeCell(QPushButton):
+
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.setMinimumSize(400, 400)
+        self.text = ''
+    
+    def captured(self):
+        return self.text
+
+    def capture(self, symbol):
+        self.text = symbol
+        self.setText(symbol)
+        self.setEnabled(False)
+
+    def reset(self):
+        self.text = ''
+        self.setText(self.text)
+        self.setEnabled(True)
+    
+    def __repr__(self):
+        return f'[{self.text}]'
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
